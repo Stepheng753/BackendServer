7053e73e1bc0380060a0be65abbaee87c58db160
stepheng753 <stepheng753@gmail.com>
2025-09-22 06:40:09 -0700

    attempt to fix refresh token func.  will test next week

--- Flask-App/TutoringCalc/apis/auth_api.py
+++ Flask-App/TutoringCalc/apis/auth_api.py
@@ -1,5 +1,6 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from google.auth.exceptions import RefreshError
 import os.path
 import pickle
 from .helpers import get_absolute_path
@@ -17,18 +18,23 @@ SCOPES = [
 def get_credentials():
     """Gets valid user credentials from storage or initiates OAuth2 flow."""
     creds = None
-
     if os.path.exists(TOKEN_PICKLE_FILE):
         with open(TOKEN_PICKLE_FILE, 'rb') as token:
             creds = pickle.load(token)
 
+    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                creds = None  # Force re-authentication
+
+        if not creds:
             flow = InstalledAppFlow.from_client_secrets_file(OAUTH_FILE, SCOPES)
             creds = flow.run_local_server(port=0)
 
+        # Save the credentials for the next run
         with open(TOKEN_PICKLE_FILE, 'wb') as token:
             pickle.dump(creds, token)
 
