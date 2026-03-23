132b21daa9c1a1e463ba46ac069497a902b9abe8
stepheng753 <stepheng753@gmail.com>
2026-03-22 22:03:05 -0700

    took away deperecated endpoints and added deployment script

--- Flask-App/app.py
+++ Flask-App/app.py
@@ -4,10 +4,6 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from index.index import render_endpoints, check_auth
-from WeddingInvitations.rsvp import rsvp_endpoint
-from NextdoorScraper.nextdoor_scraper import scrape_nextdoor_posts_endpoint, get_log_file
-from TutoringCalc.calc_pay import calc_tutoring_pay_endpoint
-from TutoringCalc.apis.auth_api import login_oauth
 
 app = Flask(__name__, template_folder='index', static_folder='static')
 CORS(app)
@@ -23,13 +19,3 @@ def index():
 @app.route("/test")
 def test():
     return {"status": "success", "message": "Hello, World!"}
-
-app.add_url_rule("/rsvp", "rsvp", rsvp_endpoint, methods=["POST"])
-
-app.add_url_rule("/scrape-nextdoor-posts", "scrape_nextdoor_posts_endpoint", scrape_nextdoor_posts_endpoint, methods=["GET"])
-
-app.add_url_rule("/get-nextdoor-log", "get_nextdoor_log", get_log_file, methods=["GET"])
-
-app.add_url_rule("/calc-tutoring-pay", "calc_tutoring_pay", calc_tutoring_pay_endpoint, methods=["GET"])
-
-app.add_url_rule("/login-oauth", "login_oauth", login_oauth, methods=["GET"])
